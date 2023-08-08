46398cd8eee07c12f7d0c57713c796335609d6d1
hailan <2522082924@qq.com>
2023-08-08 17:08:51 +0800

    可能存在无限循环或者内存泄漏等问题，导致的结果是一运行程序，电脑就卡死

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/zs/motion_planner/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/zs/motion_planner/devel;/home/zs/rp_lidar_ws/devel;/home/zs/wpr_ws/devel;/home/zs/uuuu/demo2_ws/devel;/opt/ros/noetic'.split(';') if '/home/zs/motion_planner/devel;/home/zs/rp_lidar_ws/devel;/home/zs/wpr_ws/devel;/home/zs/uuuu/demo2_ws/devel;/opt/ros/noetic' != '' else []
--- build/map_generator/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zs/motion_planner/src/map_generator
-# Build directory: /home/zs/motion_planner/build/map_generator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
