8af04252d93545bb1a068d52e734bdaf5c37bb11
hailan <2522082924@qq.com>
2023-09-01 16:02:43 +0800

    rrt 保存一下

--- build/astar_gridmap_searcher/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/zs/motion_planner/src/astar_gridmap_searcher/include".split(';') if "/home/zs/motion_planner/src/astar_gridmap_searcher/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "astar_gridmap_searcher"
-PROJECT_SPACE_DIR = "/home/zs/motion_planner/devel"
-PROJECT_VERSION = "0.0.0"
